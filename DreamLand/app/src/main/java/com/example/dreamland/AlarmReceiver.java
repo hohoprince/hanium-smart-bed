98e57f87dba9e7dfac6dbe6fae0d766140a3cc28
hohoprince <hoot0512@gmail.com>
2020-08-17 16:19:24 +0900

    알람 서비스 삭제

--- DreamLand/app/src/main/java/com/example/dreamland/AlarmReceiver.java
+++ DreamLand/app/src/main/java/com/example/dreamland/AlarmReceiver.java
@@ -4,13 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("AlarmTest", "on receive");
-        Intent serviceIntent = new Intent(context, AlarmService.class);
-        context.startService(serviceIntent);
+        Log.d("AlarmTest", "AlarmReceiver - onReceive");
+        Toast.makeText(context, "알람 울림", Toast.LENGTH_SHORT).show();
     }
 }
