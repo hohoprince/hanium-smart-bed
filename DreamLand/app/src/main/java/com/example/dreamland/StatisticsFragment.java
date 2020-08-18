a349c57e35aafeba187a508aceb10fc35701dd25
hohoprince <hoot0512@gmail.com>
2020-08-19 00:13:49 +0900

    차트의 y범위를 시간 값에 따라 변환되도록 수정

--- DreamLand/app/src/main/java/com/example/dreamland/StatisticsFragment.java
+++ DreamLand/app/src/main/java/com/example/dreamland/StatisticsFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +22,9 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class StatisticsFragment extends Fragment {
@@ -47,11 +50,15 @@ public class StatisticsFragment extends Fragment {
 
         lineChart = view.findViewById(R.id.lineChart);
         ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(1.0f, 1.0f));
-        entries.add(new Entry(2.0f, 2.0f));
-        entries.add(new Entry(3.0f, 3.0f));
-        entries.add(new Entry(4.0f, 4.0f));
-        entries.add(new Entry(5.0f, 5.0f));
+
+
+
+        for (int i = 0; i < 7; i++) {
+            Calendar c1 = Calendar.getInstance();
+            c1.add(Calendar.MINUTE, 20 * i);
+            entries.add(new Entry(i, timeToFloat(c1)));
+        }
+
 
         XAxis xAxis = lineChart.getXAxis(); // X축
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // X축 아래로
@@ -59,12 +66,12 @@ public class StatisticsFragment extends Fragment {
         xAxis.setDrawGridLines(false);
         xAxis.setGranularity(1f);
         xAxis.setGranularityEnabled(true);
-        xAxis.setTextSize(15.0f);
+        xAxis.setTextSize(12.0f);
 
         YAxis yAxis = lineChart.getAxisLeft(); // Y축
         yAxis.setTextColor(Color.GRAY);
         yAxis.setGranularity(1f);
-        yAxis.setTextSize(15.0f);
+        yAxis.setTextSize(12.0f);
         yAxis.setXOffset(15.0f);
 
         final ArrayList<String> xLabels = new ArrayList<>();
@@ -75,13 +82,15 @@ public class StatisticsFragment extends Fragment {
         xLabels.add("09-05");
         xLabels.add("09-06");
         xLabels.add("09-07");
-        final ArrayList<String> yLabels = new ArrayList<>();
-        yLabels.add("00:00");
-        yLabels.add("00:30");
-        yLabels.add("01:00");
-        yLabels.add("01:30");
-        yLabels.add("02:00");
-        yLabels.add("02:30");
+
+        final String[] yLabels = {
+                "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00",
+                "22:30", "23:00", "23:30", "00:00", "00:30", "01:00", "01:30", "02:00", "02:30",
+                "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00",
+                "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
+                "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00",
+                "16:30", "17:00", "17:30"
+        };
 
         xAxis.setValueFormatter(new IndexAxisValueFormatter() {
             @Override
@@ -93,7 +102,7 @@ public class StatisticsFragment extends Fragment {
         yAxis.setValueFormatter(new IndexAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value) {
-                return yLabels.get((int) value);
+                return yLabels[(int) value];
             }
         });
 
@@ -116,5 +125,22 @@ public class StatisticsFragment extends Fragment {
         lineChart.setData(lineData);
         lineChart.invalidate(); // refresh
 
+    }
+
+    // 입력된 시간을 차트의 y좌표 값으로 변환
+    private float timeToFloat(Calendar cal) {
+        long millis = cal.getTimeInMillis(); // 입력된 날짜의 millis
+
+        // 시간을 당일 18시로 설정
+        cal.set(Calendar.HOUR_OF_DAY, 18);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        long diff = millis - cal.getTimeInMillis(); // 입력된 날과 18시의 차
+
+        if (diff < 0) { // 음수이면 하루를 더함
+            diff += (1000 * 60 * 60 * 24);
+        }
+        return (float) diff / (1000 * 60 * 30); // 30분 간격으로 나눈 값
     }
 }
