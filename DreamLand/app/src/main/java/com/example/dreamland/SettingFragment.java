15b392b06fa9b7b78e816c82a118ff8cbc4bf332
hohoprince <hoot0512@gmail.com>
2020-09-10 01:40:10 +0900

    침대 사용자 설정값 저장

--- DreamLand/app/src/main/java/com/example/dreamland/SettingFragment.java
+++ DreamLand/app/src/main/java/com/example/dreamland/SettingFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import iammert.com.library.Status;
 import iammert.com.library.StatusView;
@@ -37,8 +39,13 @@ public class SettingFragment extends Fragment {
     View line2;
     View line3;
     RadioGroup diseaseRadioGroup;
+    ToggleButton[] positionButtons;
+
     int diseaseIndex;
-    final String[] diseaseNames = { "허리디스크", "강직성척추염", "척추관협착증", "척추전방전위증" };
+    final String[] diseaseNames = {"허리디스크", "강직성척추염", "척추관협착증", "척추전방전위증"};
+    int[] buttonIds = {R.id.positionButton0, R.id.positionButton1,
+            R.id.positionButton2, R.id.positionButton3, R.id.positionButton4, R.id.positionButton5,
+            R.id.positionButton6, R.id.positionButton7, R.id.positionButton8, R.id.positionButton9};
     String position;
 
     // Test
@@ -77,6 +84,9 @@ public class SettingFragment extends Fragment {
         button2 = (Button) view.findViewById(R.id.button2);
         editText1 = (EditText) view.findViewById(R.id.editTextTextPersonName);
         editText2 = (EditText) view.findViewById(R.id.editTextTextPersonName2);
+
+        positionButtons = new ToggleButton[10];
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -99,7 +109,6 @@ public class SettingFragment extends Fragment {
             hideDiseaseView();
         } else { // 질환 완화 모드
             diseaseIndex = sf.getInt("disease", 0); // 설정한 질환을 불러옴
-             position = sf.getString("position", "0000000000");
             tvDisease.setText(diseaseNames[diseaseIndex]);
         }
 
@@ -133,8 +142,20 @@ public class SettingFragment extends Fragment {
         bedPositionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                View dlgView = getLayoutInflater().from(getContext()).inflate(
+                final View dlgView = getLayoutInflater().from(getContext()).inflate(
                         R.layout.dialog_bed_position, null);
+                position = sf.getString("position", "0,0,0,0,0,0,0,0,0,0");
+                String[] positionArray = position.split(",");
+
+                for (int i = 0; i < 10; i++) {
+                    positionButtons[i] = (ToggleButton) dlgView.findViewById(buttonIds[i]);
+                    if (positionArray[i].equals("1")) {
+                        positionButtons[i].setChecked(true);
+                    } else {
+                        positionButtons[i].setChecked(false);
+                    }
+                }
+
                 Button completeButton = dlgView.findViewById(R.id.yesButton);
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
@@ -146,7 +167,21 @@ public class SettingFragment extends Fragment {
                 completeButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
+                        StringBuilder positionStr = new StringBuilder();
+                        for (int i = 0; i < 10; i++) {
+                            if (positionButtons[i].isChecked()) {
+                                positionStr.append("1,");
+                            } else {
+                                positionStr.append("0,");
+                            }
+                        }
+                        positionStr.deleteCharAt(positionStr.length() - 1);
+                        String savedPosition = positionStr.toString();
+                        sf.edit().putString("position", savedPosition).apply();
+                        String msg = "position:" + savedPosition;
+                        Log.d("BLT", msg);
+                        // TODO : 침대에 포지션 정보 전송
+                        dialog.dismiss();
                     }
                 });
             }
