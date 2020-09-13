734833d0f13b6ab2ba35a6af36eeffaaf20cd2c6
hohoprince <hoot0512@gmail.com>
2020-09-14 00:30:03 +0900

    지도에 마커 추가

--- DreamLand/app/src/main/java/com/example/dreamland/MapsActivity.java
+++ DreamLand/app/src/main/java/com/example/dreamland/MapsActivity.java
@@ -29,13 +29,33 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        LatLng currentLocation = new LatLng(37.344814, 126.737185);
+        LatLng hospitalLocation1 = new LatLng(37.345599, 126.736434);
+        LatLng hospitalLocation2 = new LatLng(37.345505, 126.737647);
+        LatLng hospitalLocation3 = new LatLng(37.344994, 126.738387);
+        LatLng hospitalLocation4 = new LatLng(37.345113, 126.738569);
+        LatLng hospitalLocation5 = new LatLng(37.344021, 126.737689);
+        mMap.addMarker(new MarkerOptions()
+                .position(hospitalLocation1)
+                .title("박승호 내과의원"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(hospitalLocation2)
+                .title("Marker in Sydney"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(hospitalLocation3)
+                .title("Marker in Sydney"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(hospitalLocation4)
+                .title("Marker in Sydney"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(hospitalLocation5)
+                .title("Marker in Sydney"));
 
 
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions()
-//                .position(sydney)
-//                .title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
     }
 }
