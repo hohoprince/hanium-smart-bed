be5ad2ba0af5faafa0f0bb0c39fe05e44f169f6f
hohoprince <hoot0512@gmail.com>
2020-10-01 16:12:14 +0900

    근처 병원에 마커 표시 기능 추가

--- DreamLand/app/src/main/java/com/example/dreamland/MapsActivity.java
+++ DreamLand/app/src/main/java/com/example/dreamland/MapsActivity.java
@@ -13,11 +13,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -28,13 +29,25 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+
+import noman.googleplaces.NRPlaces;
+import noman.googleplaces.Place;
+import noman.googleplaces.PlaceType;
+import noman.googleplaces.PlacesException;
+import noman.googleplaces.PlacesListener;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, PlacesListener {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationProviderClient;
@@ -42,12 +55,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private MyLocationCallback myLocationCallback;
     private final int REQUEST_ACCESS_FINE_LOCATION = 1000;
     private Context context;
+    List<Marker> previousMarkers = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         context = this;
+        previousMarkers = new ArrayList<Marker>();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -66,13 +81,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         permissionCheck();
     }
 
+    @SuppressLint("RestrictedApi")
     private void locationInit() {
         fusedLocationProviderClient = new FusedLocationProviderClient(this);
         myLocationCallback = new MyLocationCallback();
         locationRequest = new LocationRequest();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
     }
 
     @SuppressLint("MissingPermission")
@@ -128,6 +142,97 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    public String getCurrentAddress(LatLng latlng) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> addresses;
+
+        try {
+
+            addresses = geocoder.getFromLocation(
+                    latlng.latitude,
+                    latlng.longitude,
+                    1);
+        } catch (IOException ioException) {
+            //네트워크 문제
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+
+        }
+
+
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+
+        } else {
+            Address address = addresses.get(0);
+            return address.getAddressLine(0).toString();
+        }
+
+    }
+
+    @Override
+    public void onPlacesFailure(PlacesException e) {
+
+    }
+
+    @Override
+    public void onPlacesStart() {
+
+    }
+
+    @Override
+    public void onPlacesSuccess(final List<Place> places) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                for (Place place : places) {
+                    LatLng latLng = new LatLng(place.getLatitude(), place.getLongitude());
+
+                    String markerSnippet = getCurrentAddress(latLng);
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.title(place.getName());
+                    markerOptions.snippet(markerSnippet);
+                    Marker item = mMap.addMarker(markerOptions);
+                    previousMarkers.add(item);
+                }
+
+                HashSet<Marker> hashSet = new HashSet<>();
+                hashSet.addAll(previousMarkers);
+                previousMarkers.clear();
+                previousMarkers.addAll(hashSet);
+            }
+        });
+    }
+
+    @Override
+    public void onPlacesFinished() {
+
+    }
+
+    public void showPlaceInformation(LatLng latLng) {
+        mMap.clear();
+
+        if (previousMarkers != null) {
+            previousMarkers.clear();
+        }
+
+        new NRPlaces.Builder()
+                .listener(this)
+                .key("AIzaSyBNzCTzi7w-VPiSIu2PAVhMqV2eX9wdAeQ")
+                .latlng(latLng.latitude, latLng.longitude)
+                .radius(3000)
+                .type(PlaceType.HOSPITAL)
+                .language("ko", "KR")
+                .build()
+                .execute();
+    }
+
     class MyLocationCallback extends LocationCallback {
         @Override
         public void onLocationResult(LocationResult locationResult) {
@@ -135,6 +240,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Location location = locationResult.getLastLocation();
             if (location != null) {
                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude()); // 현위치
+                showPlaceInformation(latLng);
 
                 BitmapDrawable bitmapDrawable = (BitmapDrawable)getResources().getDrawable(R.drawable.current_location);
                 Bitmap b = bitmapDrawable.getBitmap();
