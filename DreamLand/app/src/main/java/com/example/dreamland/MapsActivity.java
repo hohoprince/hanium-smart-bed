6f5c3108ba681534c78e0317893d7272f4c642f8
hohoprince <hoot0512@gmail.com>
2020-10-01 12:09:40 +0900

    현위치 표시 기능 추가

--- DreamLand/app/src/main/java/com/example/dreamland/MapsActivity.java
+++ DreamLand/app/src/main/java/com/example/dreamland/MapsActivity.java
@@ -1,9 +1,27 @@
 package com.example.dreamland;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,48 +32,108 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+    private MyLocationCallback myLocationCallback;
+    private final int REQUEST_ACCESS_FINE_LOCATION = 1000;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        context = this;
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationInit();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng currentLocation = new LatLng(37.344814, 126.737185);
-        LatLng hospitalLocation1 = new LatLng(37.345599, 126.736434);
-        LatLng hospitalLocation2 = new LatLng(37.345505, 126.737647);
-        LatLng hospitalLocation3 = new LatLng(37.344994, 126.738387);
-        LatLng hospitalLocation4 = new LatLng(37.345113, 126.738569);
-        LatLng hospitalLocation5 = new LatLng(37.344021, 126.737689);
-        mMap.addMarker(new MarkerOptions()
-                .position(hospitalLocation1)
-                .title("박승호 내과의원"));
 
-        mMap.addMarker(new MarkerOptions()
-                .position(hospitalLocation2)
-                .title("Marker in Sydney"));
+    }
 
-        mMap.addMarker(new MarkerOptions()
-                .position(hospitalLocation3)
-                .title("Marker in Sydney"));
+    @Override
+    protected void onResume() {
+        super.onResume();
+        permissionCheck();
+    }
 
-        mMap.addMarker(new MarkerOptions()
-                .position(hospitalLocation4)
-                .title("Marker in Sydney"));
+    private void locationInit() {
+        fusedLocationProviderClient = new FusedLocationProviderClient(this);
+        myLocationCallback = new MyLocationCallback();
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+    }
 
-        mMap.addMarker(new MarkerOptions()
-                .position(hospitalLocation5)
-                .title("Marker in Sydney"));
+    @SuppressLint("MissingPermission")
+    private void addLocationListener() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, myLocationCallback, null);
+    }
 
+    private void showPermissionInfoDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("위치 권한").setMessage("권한이 필요함");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ActivityCompat.requestPermissions((Activity) context,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
+            }
+        }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+            }
+        }).create().show();
+    }
+
+    private void permissionCheck() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // cancel
+                showPermissionInfoDialog();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_ACCESS_FINE_LOCATION);
+            }
+        } else {
+            // 권한 수락
+            addLocationListener();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한 허용됨
+                addLocationListener();
+            } else {
+                Toast.makeText(this, "권한 거부됨", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    class MyLocationCallback extends LocationCallback {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            Location location = locationResult.getLastLocation();
+            if (location != null) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude()); // 현위치
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16f));
+            }
+        }
     }
 }
